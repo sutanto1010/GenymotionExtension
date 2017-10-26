19ad26388e98579ad7785545abd66db70c9d7c3d
Sutanto <sutanto.1010@gmail.com>
2017-10-27 01:32:00 +0700

    First Blood!

--- GenymotionExtension/App.xaml.cs
+++ GenymotionExtension/App.xaml.cs
@@ -1,17 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using GenymotionExtension.Views;
 
 namespace GenymotionExtension
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainWindow = Index.Current;
+            MainWindow.Show();
+        }
     }
 }
